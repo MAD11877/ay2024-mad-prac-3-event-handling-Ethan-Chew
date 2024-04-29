2adadd829705e54a25ff2817b0e3d989cce8e0fb
Ethan-Chew <ethanchewmh@gmail.com>
2024-04-29 14:31:20 +0800

    feat: finished practical except challenge

---
+++ app/src/main/java/sg/edu/np/mad/madpractical3/ListActivity.java
@@ -0,0 +1,63 @@
+package sg.edu.np.mad.madpractical3;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Profile");
+        builder.setMessage("MADness");
+        builder.setCancelable(true);
+        builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Random rand = new Random();
+                int randomNum = rand.nextInt(999999);
+                // Send Random Number to MainActivity as an Intent
+                Intent sendRandomNum = new Intent(ListActivity.this, MainActivity.class);
+                sendRandomNum.putExtra("num", String.valueOf(randomNum));
+                startActivity(sendRandomNum);
+            }
+        });
+        builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog alert = builder.create();
+
+        ImageView clickerImage = findViewById(R.id.clickerImage);
+        clickerImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alert.show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
@@ -0,0 +1,63 @@
+package sg.edu.np.mad.madpractical3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.textservice.TextInfo;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialise User Obj
+        User user = new User("John Doe", "MAD Developer", 1, false);
+
+        // Get Components from Layout
+        TextView tvName = findViewById(R.id.tvName);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+        Button btnFollow = findViewById(R.id.btnFollow);
+
+        // Get Data from Intent
+        Intent receivingIntent = getIntent();
+        String randNum = receivingIntent.getStringExtra("num");
+
+        // Set Components with Values
+        String tvNameText = user.name + " " + randNum;
+        tvName.setText(tvNameText);
+        tvDescription.setText(user.description);
+        btnFollow.setText("Follow");
+
+        // OnClickListener for Follow Button
+        btnFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user.followed) {
+                    btnFollow.setText("Unfollow");
+                    Toast.makeText(getApplicationContext(), "Followed", Toast.LENGTH_LONG).show();
+                    user.followed = false;
+                } else {
+                    btnFollow.setText("Follow");
+                    Toast.makeText(getApplicationContext(), "Unfollow", Toast.LENGTH_LONG).show();
+                    user.followed = true;
+                }
+            }
+        });
+    }
+}
